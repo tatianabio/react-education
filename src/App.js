6726d0a3893eb11355c66d2dc4e347dbe9250cb5
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-11-13 17:09:33 +0300

    Creating a list of posts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './styles/App.css';
-import PostItem from './components/PostItem';
+import PostList from "./components/PostList";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Java Script', body: 'Description' },
-    { id: 2, title: 'Java Script 2', body: 'Description' },
-    { id: 3, title: 'Java Script 3', body: 'Description' },
-  ]);
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'Java Script', body: 'Description' },
+        { id: 2, title: 'Java Script 2', body: 'Description' },
+        { id: 3, title: 'Java Script 3', body: 'Description' },
+    ]);
 
   return (
     <div className="App">
-      <h1>Список постов</h1>
-      {posts.map((post) => (
-        <PostItem post={post} key={post.id} />
-      ))}
+      <PostList posts={posts} title={'Посты про JS'}/>
     </div>
   );
 }
